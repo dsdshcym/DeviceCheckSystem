b39e4e5fa8cfca40022b09bb7de136346bbbdc4c
dsdshcym <dsdshcym@gmail.com>
2016-06-07 09:33:38 +0800

    Add has_many association to Device and CheckPlan

--- app/models/check_plan.rb
+++ app/models/check_plan.rb
@@ -1,3 +1,4 @@
 class CheckPlan < ActiveRecord::Base
   belongs_to :device_type
+  has_many :devices, through: :device_type
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,5 @@
 class Device < ActiveRecord::Base
   belongs_to :device_type
   has_many :device_plans
+  has_many :check_plans, through: :device_type
 end
