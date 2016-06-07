b5ef87aaf23952cc5de036f16c3f0201c6b93e57
dsdshcym <dsdshcym@gmail.com>
2016-06-07 11:01:18 +0800

    Add generate device plans to device#show template

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :devices
   resources :device_types
 
-  post 'devices/:id/generate' => 'devices#generate'
+  post 'devices/:id/generate', to: 'devices#generate', as: 'generate'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
